12058c379c6718ae4d4710732fd92a505bd84cdc
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-06-02 01:23:41 +0530

    Create quotes.js

---
+++ app/data/quotes.js
@@ -0,0 +1,17 @@
+export const inspirationalQuotes = [
+  {
+    id: 1,
+    quote: "The mind is everything. What you think you become.",
+    author: "Buddha"
+  },
+  {
+    id: 2,
+    quote: "Everything can be taken from a man but one thing: the freedom to choose one's attitude.",
+    author: "Viktor Frankl"
+  },
+  {
+    id: 3,
+    quote: "The curious paradox is that when I accept myself just as I am, then I can change.",
+    author: "Carl Rogers"
+  }
+];
